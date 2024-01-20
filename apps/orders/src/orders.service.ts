d5889bab640dc5419292174c06b7b424f077d6bc
Kenneth <deca@DECAGONs-MacBook-Air.local>
2024-01-20 10:20:40 +0100

    added rabbitmq to the billing and order service

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateOrderRequest } from './dto/create-order.request';
+import { OrdersRepository } from './orders.repository';
+import { BILLING_SERVICE } from './constants/services';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class OrdersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly orderRepository: OrdersRepository,
+    @Inject(BILLING_SERVICE) private billingClient: ClientProxy,
+  ) {}
+
+  async createOrder(request: CreateOrderRequest) {
+    return this.orderRepository.create(request);
+  }
+
+  async getOrders() {
+    return this.orderRepository.find({});
   }
 }
