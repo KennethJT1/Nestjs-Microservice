e5d6ed71526a16ae6f9b6c9449feed5c3285d670
Kenneth <deca@DECAGONs-MacBook-Air.local>
2024-01-23 19:32:32 +0100

    add to order and  billing-service

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -3,6 +3,7 @@ import { CreateOrderRequest } from './dto/create-order.request';
 import { OrdersRepository } from './orders.repository';
 import { BILLING_SERVICE } from './constants/services';
 import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class OrdersService {
@@ -12,7 +13,22 @@ export class OrdersService {
   ) {}
 
   async createOrder(request: CreateOrderRequest) {
-    return this.orderRepository.create(request);
+    const session = await this.orderRepository.startTransaction();
+    try {
+      const order = await this.orderRepository.create(request, { session });
+
+      await lastValueFrom(
+        this.billingClient.emit('order_created', {
+          request,
+          // Authentication: authentication,
+        }),
+      );
+      await session.commitTransaction();
+      return order;
+    } catch (error) {
+      await session.abortTransaction();
+      throw error;
+    }
   }
 
   async getOrders() {
